7b6d94730899770dd74596fc186f2a9f00b8de98
SZkristof <szabokristof0@gmail.com>
2017-05-16 14:07:32 +0200

    Menu extended with 4th option

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     print("Press number 1 for mentor names!")
     print("Press number 2 for mentor nicknames!")
     print("Press number 3 for applicant names!")
+    print("Press number 4 for lost hat girl!")
     user_choice = input("")
     if int(user_choice) == 1:
         print("\n")
@@ -15,6 +16,9 @@ def main():
     elif int(user_choice) == 3:
         print("\n")
         return queries.finding_carol()
+    elif int(user_choice) == 4:
+        print("\n")
+        return queries.the_lost_hat()
 
 
 if __name__ == '__main__':
