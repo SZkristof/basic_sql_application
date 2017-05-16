c2eb2d19448268a60cb938eb58de78df1d9becf6
SZkristof <szabokristof0@gmail.com>
2017-05-17 00:41:04 +0200

    Main menu created, simple but no clean code..yet

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     print("Press number 2 for mentor nicknames!")
     print("Press number 3 for applicant names!")
     print("Press number 4 for lost hat girl!")
-    user_choice = input("")
+    print("Press number 5 for Markus, the new applicant!")
+    print("Press number 6 for telephone number change!")
+    print("Press number 7 for deleting applicants!")
+    user_choice = input("Give: ")
     if int(user_choice) == 1:
         print("\n")
         return queries.mentor_names()
@@ -19,7 +22,16 @@ def main():
     elif int(user_choice) == 4:
         print("\n")
         return queries.the_lost_hat()
-
+    elif int(user_choice) == 5:
+        print("\n")
+        return queries.new_applicant()
+    elif int(user_choice) == 6:
+        print("\n")
+        return queries.change_phone_number()
+    elif int(user_choice) == 7:
+        print("\n")
+        return queries.delete_applicants()
 
 if __name__ == '__main__':
     main()
+
