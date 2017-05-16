d3708e76b5b3748ac0ccada992749db3f7f3de73
SZkristof <szabokristof0@gmail.com>
2017-05-16 02:48:59 +0200

    Third menu stage added to main menu

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import queries
 def main():
     print("Press number 1 for mentor names!")
     print("Press number 2 for mentor nicknames!")
+    print("Press number 3 for applicant names!")
     user_choice = input("")
     if int(user_choice) == 1:
         print("\n")
@@ -11,6 +12,9 @@ def main():
     elif int(user_choice) == 2:
         print("\n")
         return queries.mentor_nicks()
+    elif int(user_choice) == 3:
+        print("\n")
+        return queries.finding_carol()
 
 
 if __name__ == '__main__':
