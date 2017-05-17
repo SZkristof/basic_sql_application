7eb3c678c2ca20ba78bce61e85a9aa9eca1ad90f
SZkristof <szabokristof0@gmail.com>
2017-05-17 11:38:11 +0200

    Print functions created, seperately from the menu

---
+++ printing.py
@@ -0,0 +1,13 @@
+def menu():
+    print("Press number 1 for mentor names!")
+    print("Press number 2 for mentor nicknames!")
+    print("Press number 3 for applicant names!")
+    print("Press number 4 for lost hat girl!")
+    print("Press number 5 for Markus, the new applicant!")
+    print("Press number 6 for telephone number change!")
+    print("Press number 7 for deleting applicants!")
+    print("\n" + "Type 'q' to exit from the menu..")
+
+
+def typo():
+    print("Please choose from 1-7 or press 'q' to exit: ")
