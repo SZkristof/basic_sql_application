82f4aa435d08f0d2247058178fc408424fb28547
SZkristof <szabokristof0@gmail.com>
2017-05-17 11:40:55 +0200

    Structural reorganisation of the main menu: while loop added with quit option as well

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import queries
+import printing
 
 
 def main():
-    print("Press number 1 for mentor names!")
-    print("Press number 2 for mentor nicknames!")
-    print("Press number 3 for applicant names!")
-    print("Press number 4 for lost hat girl!")
-    print("Press number 5 for Markus, the new applicant!")
-    print("Press number 6 for telephone number change!")
-    print("Press number 7 for deleting applicants!")
-    user_choice = input("Give: ")
-    if int(user_choice) == 1:
-        print("\n")
-        return queries.mentor_names()
-    elif int(user_choice) == 2:
-        print("\n")
-        return queries.mentor_nicks()
-    elif int(user_choice) == 3:
-        print("\n")
-        return queries.finding_carol()
-    elif int(user_choice) == 4:
-        print("\n")
-        return queries.the_lost_hat()
-    elif int(user_choice) == 5:
-        print("\n")
-        return queries.new_applicant()
-    elif int(user_choice) == 6:
-        print("\n")
-        return queries.change_phone_number()
-    elif int(user_choice) == 7:
-        print("\n")
-        return queries.delete_applicants()
+    printing.menu()
+    running = True
+    while running:
+        user_choice = input("Choose a number from 1 to 7: ")
+        if user_choice == "1":
+            return queries.mentor_names()
+        elif user_choice == "2":
+            return queries.mentor_nicks()
+        elif user_choice == "3":
+            return queries.finding_carol()
+        elif user_choice == "4":
+            return queries.the_lost_hat()
+        elif user_choice == "5":
+            return queries.new_applicant()
+        elif user_choice == "6":
+            return queries.change_phone_number()
+        elif user_choice == "7":
+            return queries.delete_applicants()
+        elif user_choice == "q":
+            break
+        else:
+            printing.typo()
+
 
 if __name__ == '__main__':
     main()
